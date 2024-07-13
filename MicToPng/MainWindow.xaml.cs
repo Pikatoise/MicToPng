eb8b8ef266892c0f0ff8186738dc4d1711c754d7
Pikatoise <wierwolf@inbox.ru>
2024-07-13 16:39:21 +0500

    Add window controls

--- MicToPng/MainWindow.xaml.cs
+++ MicToPng/MainWindow.xaml.cs
@@ -1,24 +1,28 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MicToPng
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow: Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonMinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void ButtonCloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BorderWindowDragZone_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
         }
     }
 }
