4b3c1251d865a143951b4ea99f7503f35325a245
Pikatoise <wierwolf@inbox.ru>
2024-07-17 01:10:51 +0500

    Add doc for library

--- MicToPngConverter/obj/Debug/net8.0/MicToPngConverter.AssemblyInfo.cs
+++ MicToPngConverter/obj/Debug/net8.0/MicToPngConverter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicToPngConverter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39758bc52ad4cd8f7ac5e59d62a3eed61389c256")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52dddb84459596fa39899965e31f5b9d98427508")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicToPngConverter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicToPngConverter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
