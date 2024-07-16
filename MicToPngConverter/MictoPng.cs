52dddb84459596fa39899965e31f5b9d98427508
Pikatoise <wierwolf@inbox.ru>
2024-07-17 01:09:09 +0500

    Init MicToPngConverted library

---
+++ MicToPngConverter/MictoPng.cs
@@ -0,0 +1,24 @@
+﻿namespace MicToPngConverter
+{
+    public class MicToPng
+    {
+        string _path;
+        string _outputPath;
+        int _convertedFiles = 0;
+
+        public delegate void ProgressHandler(int progress);
+        public event ProgressHandler? OnProgressChanged;
+
+        public MicToPng(string path)
+        {
+            _path = path;
+            _outputPath = path + "/converted/";
+        }
+
+        public MicToPng(string path, string outputPath)
+        {
+            _path = path;
+            _outputPath = outputPath;
+        }
+    }
+}
