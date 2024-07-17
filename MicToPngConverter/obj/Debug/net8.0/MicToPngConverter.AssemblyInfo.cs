6c3c761da2ed0667b7e9751f5f8d73d96894bd4c
Pikatoise <wierwolf@inbox.ru>
2024-07-18 00:32:38 +0500

    build

--- MicToPngConverter/obj/Debug/net8.0/MicToPngConverter.AssemblyInfo.cs
+++ MicToPngConverter/obj/Debug/net8.0/MicToPngConverter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicToPngConverter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f9c00205b83bbb531e34377a5d65b6401b68993")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ef7a5dc06313d727259a20cba6ec8b9afa194b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicToPngConverter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicToPngConverter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
